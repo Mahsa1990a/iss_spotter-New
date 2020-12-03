0f4ee5bbe4a925a608b353344e7f8a655e86140c
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-03 23:46:21 +0000

    func2

--- index2.js
+++ index2.js
@@ -1,7 +1,9 @@
-const { fetchMyIP } = require("./iss_promised");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss_promised");
 
 fetchMyIP()
+.then(fetchCoordsByIP)
 .then(body => console.log(body));
-
+//body will now be the response body (JSON string) returned from our second API call, to ipvigilante,
+//containing geographical information for the given IP address.
 
 
